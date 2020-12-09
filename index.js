8e371b9b60fa052a5f1db4c90f9745e22a11ee6a
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-12-09 20:50:08 -0300

    cambio numeros en string como numeros

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const user = {
     firstName: "Macarena",
     lastName: "Reale Portelli",
     email: "macarenarealeportelli@gmail.com",
-    age: "30",
+    age: 30,
 }
 
 console.log(`Hola, mi nombre es ${user.firstName} ${user.lastName} y tengo ${user.age} años`)
@@ -12,38 +12,38 @@ console.log(`Hola, mi nombre es ${user.firstName} ${user.lastName} y tengo ${use
 // Ejercicio 2
 
 const usuario1 = {
-    id: '1',
+    id: 1,
     nombre: 'Ada Lovelace',
     email: 'ada@gmail.com',
-    telefono: '1234567890',
+    telefono: 1234567890,
 }
 
 const usuario2 = {
-    id: '2',
+    id: 2,
     nombre: 'Grace Cooper',
     email: 'grace@hotmail.com',
-    telefono: '0987654321',
+    telefono: 0987654321,
 }
 
 const usuario3 = {
-    id: '3',
+    id: 3,
     nombre: 'Hedy Lamar',
     email: 'hedy@gmail.com',
-    telefono: '6789054321',
+    telefono: 6789054321,
 }
 
 const usuario4 = {
-    id: '4',
+    id: 4,
     nombre: 'Radia Perlman',
     email: 'radia@yahoo.com',
-    telefono: '1234509876',
+    telefono: 1234509876,
 }
 
 const usuario5 = {
-    id: '5',
+    id: 5,
     nombre: 'Sheryl Sandberg',
     email: 'sheryl@facebook.com',
-    telefono: '5432167890',
+    telefono: 5432167890,
 }
 
 console.log(usuario1)
@@ -80,7 +80,7 @@ console.log(`El disco ${disco.nombre} de la banda ${disco.banda.nombre} se lanz�
 
 const tecnologíasConocidas = ['HTML' , 'CSS' , 'JavaScript']
 
-user.sabeProgramar = 'true'
+user.sabeProgramar = true
 user.tecnologíasConocidas = tecnologíasConocidas
 
 console.log(user)
